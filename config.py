05f7f36edde3cfc80f29a948e539c7d8dd984f52
ckh3455 <68548169+ckh3455@users.noreply.github.com>
2025-11-04 19:08:28 +0900

    config.py 경로를 실제 OneDrive 경로로 수정하고 PROPERTY_TYPES를 download_realdata.py와 일치시킴

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ IS_CI = os.getenv("CI", "") == "1"
 
 # 다운로드 경로
 if IS_CI:
-    DOWNLOAD_DIR = Path("./output")
+    DOWNLOAD_DIR = Path("./_downloads")
 else:
-    DOWNLOAD_DIR = Path(r"C:\Users\USER\OneDrive\office work\부동산 실거래 데이터")
+    DOWNLOAD_DIR = Path(r"D:\OneDrive\office work\부동산 실거래 데이터")
 
 # 임시 다운로드 폴더
 TEMP_DOWNLOAD_DIR = Path("./_temp_downloads")
@@ -18,16 +18,16 @@ TEMP_DOWNLOAD_DIR = Path("./_temp_downloads")
 # 국토부 사이트 URL
 MOLIT_URL = "https://rt.molit.go.kr/pt/xls/xls.do?mobileAt="
 
-# 다운로드할 부동산 종목
+# 다운로드할 부동산 종목 (download_realdata.py와 일치)
 PROPERTY_TYPES = [
     "아파트",
-    "연립/다세대",
-    "단독/다가구",
+    "연립다세대",
+    "단독다가구",
     "오피스텔",
     "토지",
-    "분양/입주권",
-    "상업/업무용",
-    "공장/창고 등"
+    "상업업무용",
+    "분양권",
+    "입주권"
 ]
 
 # 타임아웃 설정
