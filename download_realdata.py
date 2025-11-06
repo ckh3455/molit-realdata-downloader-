ef18ad5bbab3081365bf24ab09df27932a12c5af
ckh3455 <68548169+ckh3455@users.noreply.github.com>
2025-11-06 22:02:40 +0900

    Update download_realdata.py

--- download_realdata.py
+++ download_realdata.py
@@ -110,8 +110,9 @@ def sanitize_folder_name(name: str) -> str:
 def build_driver():
     """크롬 드라이버 생성"""
     opts = Options()
-    if IS_CI:
-        opts.add_argument("--headless=new")
+    # headless 모드 비활성화 - 브라우저 창 보이기
+    # if IS_CI:
+    #     opts.add_argument("--headless=new")
     opts.add_argument("--no-sandbox")
     opts.add_argument("--disable-dev-shm-usage")
     opts.add_argument("--disable-gpu")
